08990ccf05501c04dbb2640d02fddb75c33e1ef2
AugustoMagalhaes <augustommg@gmail.com>
2026-05-25 03:01:52 -0300

    chore: update handler

--- src/clj/tempus_fugit/handler.clj
+++ src/clj/tempus_fugit/handler.clj
@@ -1,15 +1,16 @@
 (ns tempus-fugit.handler
   (:require
-    [tempus-fugit.middleware :as middleware]
-    [tempus-fugit.layout :refer [error-page]]
-    [tempus-fugit.routes.home :refer [home-routes]]
-    [tempus-fugit.routes.services :refer [service-routes]]
-    [reitit.swagger-ui :as swagger-ui]
-    [reitit.ring :as ring]
-    [ring.middleware.content-type :refer [wrap-content-type]]
-    [ring.middleware.webjars :refer [wrap-webjars]]
-    [tempus-fugit.env :refer [defaults]]
-    [mount.core :as mount]))
+   [tempus-fugit.middleware :as middleware]
+   [tempus-fugit.layout :refer [error-page]]
+   [tempus-fugit.routes.home :refer [home-routes]]
+   [tempus-fugit.routes.auth :refer [auth-routes]]
+   [tempus-fugit.routes.services :refer [service-routes]]
+   [reitit.swagger-ui :as swagger-ui]
+   [reitit.ring :as ring]
+   [ring.middleware.content-type :refer [wrap-content-type]]
+   [ring.middleware.webjars :refer [wrap-webjars]]
+   [tempus-fugit.env :refer [defaults]]
+   [mount.core :as mount]))
 
 (mount/defstate init-app
   :start ((or (:init defaults) (fn [])))
@@ -19,29 +20,29 @@
   ([_] nil)
   ([_ respond _] (respond nil)))
 
-
 (mount/defstate app-routes
   :start
   (ring/ring-handler
-    (ring/router
-      [(home-routes)
-       (service-routes)])
-    (ring/routes
-      (swagger-ui/create-swagger-ui-handler
-        {:path   "/swagger-ui"
-         :url    "/api/swagger.json"
-         :config {:validator-url nil}})
-      (ring/create-resource-handler
-        {:path "/"})
-      (wrap-content-type
-        (wrap-webjars async-aware-default-handler))
-      (ring/create-default-handler
-        {:not-found
-         (constantly (error-page {:status 404, :title "404 - Page not found"}))
-         :method-not-allowed
-         (constantly (error-page {:status 405, :title "405 - Not allowed"}))
-         :not-acceptable
-         (constantly (error-page {:status 406, :title "406 - Not acceptable"}))}))))
+   (ring/router
+    [(home-routes)
+     (auth-routes)
+     (service-routes)])
+   (ring/routes
+    (swagger-ui/create-swagger-ui-handler
+     {:path   "/swagger-ui"
+      :url    "/api/swagger.json"
+      :config {:validator-url nil}})
+    (ring/create-resource-handler
+     {:path "/"})
+    (wrap-content-type
+     (wrap-webjars async-aware-default-handler))
+    (ring/create-default-handler
+     {:not-found
+      (constantly (error-page {:status 404, :title "404 - Page not found"}))
+      :method-not-allowed
+      (constantly (error-page {:status 405, :title "405 - Not allowed"}))
+      :not-acceptable
+      (constantly (error-page {:status 406, :title "406 - Not acceptable"}))}))))
 
 (defn app []
   (middleware/wrap-base #'app-routes))
