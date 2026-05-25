fa714fb5f872aa582b7fe6f25bc423a57550403d
AugustoMagalhaes <augustommg@gmail.com>
2026-05-24 22:53:39 -0300

    chore: add docker, docker-compose, makefile and fix re-frame bootstrap

--- src/cljs/tempus_fugit/core.cljs
+++ src/cljs/tempus_fugit/core.cljs
@@ -1,12 +1,17 @@
 (ns tempus-fugit.core
-  (:require [reagent.dom :as rdom]
+  (:require [reagent.dom.client :as rdom]
             [re-frame.core :as rf]
             [tempus-fugit.events]
+            [tempus-fugit.subs]
             [tempus-fugit.views.index :as views]))
+
+(defonce root (atom nil))
 
 (defn mount-components []
   (rf/clear-subscription-cache!)
-  (rdom/render [views/main-panel] (.getElementById js/document "app")))
+  (when-not @root
+    (reset! root (rdom/create-root (.getElementById js/document "app"))))
+  (rdom/render @root [views/main-panel]))
 
 (defn init! []
   (rf/dispatch-sync [:initialize-db])
